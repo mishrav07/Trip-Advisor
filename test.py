21cc353ca0d25ffbf97cedd3f8ddc08e2ddd9c26
mishrav07 <vikasmishra218@gmail.com>
2026-08-06 16:28:30 +0200

    backandfrontend added

--- test.py
+++ test.py
@@ -3,8 +3,10 @@ from tools.tavily_tool import tavily_search
 # res = tavily_search("Python programming")
 # print(res)
 
-from tools.flight_tool import search_flights
+from tools.backend import search_flights, get_database_url
 
+res = get_database_url()
+print(res)
 
 # res = search_flights("Plan a 7 days trip Germany to France")
 # print(res)
@@ -13,8 +15,8 @@ from tools.flight_tool import search_flights
 # Testing the backend functions
 from tools.backend import run_travel_agent
 
-res = run_travel_agent(
-    user_input="Plan a 7 days trip from Germany to France, including flights, hotels, and a detailed itinerary.",
-    thread_id="test_thread_2",
-)
-print(res["answer"])
+# res = run_travel_agent(
+#     user_input="Plan a 7 days trip from Germany to France, including flights, hotels, and a detailed itinerary.",
+#     thread_id="test_thread_2",
+# )
+# print(res["answer"])
